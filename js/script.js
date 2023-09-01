2277185dfbeec6bd98fb3919916b59a2dffa4d3b
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-09-01 10:31:26 +0600

    category button added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+
+
+const loadCategory = async () => {
+  const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+  const data = await response.json();
+  const categories = data.data;
+  console.log(categories);
+
+  // 1. grab the container 
+  const categoryContainer = document.getElementById('category-container');
+
+  categories.forEach(category => {
+    // 2. create new element 
+    const div = document.createElement('div');
+    // 3. setting the element 
+    
+    div.innerHTML = `
+      <button onclick = "changeColor()" class="btn">${category.category}</button>
+    `;
+    console.log(category.category);
+
+    // 4. appending the element 
+    categoryContainer.appendChild(div);
+
+  });
+}
+
+loadCategory();
+
+
+
+
+
+
