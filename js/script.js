22bc6ee31f729811daadbb1d0e1954438f3b5255
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-09-01 20:33:15 +0600

    card section completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const loadCategory = async () => {
   const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
   const data = await response.json();
   const categories = data.data;
-  console.log(categories);
+
 
   // 1. grab the container 
   const categoryContainer = document.getElementById('category-container');
@@ -13,19 +13,159 @@ const loadCategory = async () => {
     // 2. create new element 
     const div = document.createElement('div');
     // 3. setting the element 
-    
+
     div.innerHTML = `
-      <button onclick = "changeColor()" class="btn">${category.category}</button>
+      <button onclick = "loadVideo(${category.category_id})" class="btn">${category.category}</button>
     `;
-    console.log(category.category);
+
 
     // 4. appending the element 
     categoryContainer.appendChild(div);
 
   });
+  loadAllVideo();
+}
+
+
+const loadVideo = async (id) => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+
+  const data = await response.json();
+  const videos = data.data;
+
+  // 1. grab the container for no Data
+  const noDataContainer = document.getElementById('no-data-container');
+  // clearing the data for each load
+  noDataContainer.textContent = '';
+
+  // 2.1 grab the container for data 
+  const cardContainer = document.getElementById('card-container');
+  // clearing the data for each load 
+  cardContainer.textContent = '';
+
+  if (videos.length) {
+    videos.forEach(video => {
+      // 2.2 create new element 
+      const div = document.createElement('div');
+      // 2.3 adding inner html 
+      div.innerHTML = `
+        <div class="card card-compact">
+        <figure class="rounded-lg">
+        <img  src="${video.thumbnail}"
+            alt="Shoes" /></figure>
+        <div class="mt-5">
+          <div class="flex  gap-2">
+            <div class="avatar">
+              <div class="w-12 rounded-full">
+                <img src="${video.authors[0].profile_picture}" />
+              </div>
+            </div>
+            <div class="">
+              <h1 class="text-base font-bold">${video.title} </h1>
+            </div>
+
+          </div>
+          <div class="pl-14 -mt-5 ">
+            <div class="flex gap-1">
+              <p>${video.authors[0].profile_name}</p>
+              <span>${video.authors[0].verified ? "<img src='image/verified.png'>" : ""}</span>
+              
+            </div>
+          
+            <p>${video.others.views}</p>
+          </div>
+          
+          <div class="card-actions">
+
+
+          </div>
+        </div>
+      </div>
+      `;
+      // 2.4 append the element 
+      cardContainer.appendChild(div);
+    })
+  }
+  else {
+    // 2. create element 
+    const div = document.createElement('div');
+
+    // 3. setting inner content 
+    div.innerHTML = `
+      <div class="flex justify-center items-center">
+        <img src="image/Icon.png" alt="">
+      </div>
+      <div class="flex justify-center items-center">
+        <p class="text-center text-4xl font-bold mt-8">Oops!! Sorry, There is no <br> content here</p>
+      </div>
+    `;
+
+    // 4. append the child 
+    noDataContainer.appendChild(div);
+  }
+}
+
+
+
+const loadAllVideo = async () => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
+
+  const data = await response.json();
+  const videos = data.data;
+
+
+  // 1 grab the container for data 
+  const cardContainer = document.getElementById('card-container');
+  // clearing the data for each load 
+  cardContainer.textContent = '';
+
+  videos.forEach(video => {
+    console.log(video.authors[0].verified);
+    // 2. create new element 
+    const div = document.createElement('div');
+    // 3. adding inner html 
+    div.innerHTML = `
+        <div class="card card-compact">
+        <figure class="rounded-lg">
+        <img  src="${video.thumbnail}"
+            alt="Shoes" /></figure>
+        <div class="mt-5">
+          <div class="flex  gap-2">
+            <div class="avatar">
+              <div class="w-12 rounded-full">
+                <img src="${video.authors[0].profile_picture}" />
+              </div>
+            </div>
+            <div class="">
+              <h1 class="text-base font-bold">${video.title} </h1>
+            </div>
+
+          </div>
+          <div class="pl-14 -mt-5 ">
+            <div class="flex gap-1">
+              <p>${video.authors[0].profile_name}</p>
+              <span>${video.authors[0].verified ? "<img src='image/verified.png'>" : ""}</span>
+              
+            </div>
+          
+            <p>${video.others.views}</p>
+          </div>
+          
+          <div class="card-actions">
+
+
+          </div>
+        </div>
+      </div>
+      `;
+    // 4. append the element 
+    cardContainer.appendChild(div);
+  })
 }
 
 loadCategory();
+
+
 
 
 
